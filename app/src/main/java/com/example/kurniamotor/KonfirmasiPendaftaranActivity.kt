585c58d2d3367051abcc5cf3c3b75239851baff0
Messy Wirdianti <mesiwirdianti@gmail.com>
2023-11-21 20:31:35 +0700

    tes

---
+++ app/src/main/java/com/example/kurniamotor/KonfirmasiPendaftaranActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kurniamotor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KonfirmasiPendaftaranActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konfirmasi_pendaftaran)
+    }
+}
