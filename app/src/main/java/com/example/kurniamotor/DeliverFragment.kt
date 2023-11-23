ab8d143e6269453cf302a7201fbbf72ac7e2bcef
Reza Yahya <yayapahlevi@gmail.com>
2023-11-23 12:11:13 +0700

    Tambah halaman setting dan antar jemput

--- app/src/main/java/com/example/kurniamotor/DeliverFragment.kt
+++ app/src/main/java/com/example/kurniamotor/DeliverFragment.kt
@@ -1,10 +1,14 @@
 package com.example.kurniamotor
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
 class DeliverFragment : Fragment() {
 
     override fun onCreateView(
@@ -12,7 +16,14 @@ class DeliverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deliver, container, false)
+        val view =  inflater.inflate(R.layout.fragment_deliver, container, false)
+
+        val btn_daftarLayanan = view.findViewById<Button>(R.id.btn_daftarLayanan)
+        btn_daftarLayanan.setOnClickListener {
+            val i = Intent(this@DeliverFragment.context, PesanAntarJemput::class.java)
+            startActivity(i)
+        }
+        return view
     }
 
     companion object {
--- app/src/main/java/com/example/kurniamotor/SettingsFragment.kt
+++ app/src/main/java/com/example/kurniamotor/SettingsFragment.kt
@@ -1,18 +1,41 @@
 package com.example.kurniamotor
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val btn_upprofile = view.findViewById<Button>(R.id.btn_upprofile)
+        btn_upprofile.setOnClickListener {
+            val i = Intent(this@SettingsFragment.context, EditProfil::class.java)
+            startActivity(i)
+        }
+        val btn_about = view.findViewById<Button>(R.id.btn_about)
+        btn_about.setOnClickListener {
+            val i = Intent(this@SettingsFragment.context, TentangKami::class.java)
+            startActivity(i)
+        }
+        val btn_hub = view.findViewById<Button>(R.id.btn_hub)
+        btn_hub.setOnClickListener {
+            val i = Intent(this@SettingsFragment.context, HubungiKami::class.java)
+            startActivity(i)
+        }
+        val btn_keluar = view.findViewById<Button>(R.id.btn_keluar)
+        btn_keluar.setOnClickListener {
+            val i = Intent(this@SettingsFragment.context, LoginActivity::class.java)
+            startActivity(i)
+        }
+        return view
     }
 
     companion object {
