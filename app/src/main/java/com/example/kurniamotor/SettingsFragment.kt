9a2b900fd7ad627b5e1ddb1690ad534c8f26435f
GalungWendiWijaya <galung.21421019@student.ubl.ac.id>
2023-11-23 08:05:03 +0700

    add fragment home & lihat pesanan

--- app/src/main/java/com/example/kurniamotor/SettingsFragment.kt
+++ app/src/main/java/com/example/kurniamotor/SettingsFragment.kt
@@ -12,7 +12,7 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.settingfragment, container, false)
     }
 
     companion object {
